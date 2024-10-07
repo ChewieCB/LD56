36269e180ebc0f77153311bd963bdfd32ab9b904
NKLam <khoalamvn@gmail.com>
2024-10-08 05:08:48 +1100

    Make UI look fancier

--- src/ui/common/styled_button/animated_button.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-class_name StyledButton
-
-
-func _ready() -> void:
-	# SFX hooks
-	self.mouse_entered.connect(GlobalSFX.play_button_hover_sfx)
-	self.pressed.connect(GlobalSFX.play_button_click_sfx)
