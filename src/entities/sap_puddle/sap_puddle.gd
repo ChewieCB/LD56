4ea6fd630204ce819cc0f732870b06cc5901accb
Jack McCaffrey <jackmccaffrey96@gmail.com>
2024-10-07 11:46:27 +0100

    Made sap spawn agents based on export var

--- src/entities/sap_puddle/sap_puddle.gd
+++ src/entities/sap_puddle/sap_puddle.gd
@@ -7,13 +7,23 @@ class_name SapPuddle
 
 @onready var puddle_mesh := $PuddleMesh
 @onready var collider: CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D
-@onready var agent_markers: Node2D = $AgentMarkers
+@onready var agent_markers_node: Node2D = $AgentMarkers
+@onready var agent_marker_scene: PackedScene = preload("res://src/entities/sap_puddle/AgentMarker.tscn")
 var is_consumed: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var offset := Vector2(-8.0, -8.0)
+	var offset_polygon: PackedVector2Array = collider.polygon * Transform2D(0, offset)
+	var random_point_gen := PolygonRandomPointGenerator.new(offset_polygon)
+	for agent in agents_count:
+		var marker = agent_marker_scene.instantiate()
+		var marker_pos := Vector2.INF
+		while not Geometry2D.is_point_in_polygon(marker_pos, collider.polygon):
+			marker_pos = random_point_gen.get_random_point()
+		marker.position = marker_pos
+		agent_markers_node.add_child(marker)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -37,7 +47,7 @@ func regenerate() -> void:
 		4.0
 	).set_trans(Tween.TRANS_LINEAR)
 	tween.chain()
-	for marker in agent_markers.get_children():
+	for marker in agent_markers_node.get_children():
 		var marker_sprite: Sprite2D = marker.get_node("SwarmSprite")
 		tween.tween_property(
 			marker_sprite,
@@ -66,7 +76,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 			collider.call_deferred("set_disabled", true)
 			is_consumed = true
 			
-			for marker: Marker2D in agent_markers.get_children():
+			for marker: Marker2D in agent_markers_node.get_children():
 				GameManager.swarm_director.add_agent(marker.global_position)
 			
 			var tween = get_tree().create_tween()
@@ -83,7 +93,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 				0.5,
 				0.25
 			).set_trans(Tween.TRANS_LINEAR)
-			for marker in agent_markers.get_children():
+			for marker in agent_markers_node.get_children():
 				tween.tween_property(
 					marker.get_node("SwarmSprite"),
 					"modulate",
